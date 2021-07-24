8bb4dddf4ff07ea2a92bbcc13be220f0f90bc9d2
jtlantz <jtlantz1995@gmail.com>
2021-07-24 09:03:04 +0000

    Added some random number generation

---
+++ random-num/gen.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<pthread.h>
+
+#include "gen.h"
+
+int main(int argc, char argv[])
+{
+    FILE *in1, *in2;
+    in1 = fopen("./input1.in","w");
+	in2 = fopen("./input2.in","w");
+    int i, j;
+
+    for(i = 0; i < 20000;i++){
+        for(j = 0; j<20000; j++){
+            fprintf(in1, "%ld ", random());
+            fprintf(in2, "%ld ", random());
+        }
+        fprintf(in1, "\n");
+        fprintf(in2, "\n");
+    }
+    fclose(in1);
+    fclose(in2);
+}
